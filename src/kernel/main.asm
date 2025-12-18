279dd5c0770468406463531eb6273240fe18b941
CrimsonOnGit-hub <allaboutwaterdiamond@gmail.com>
2025-12-18 17:51:11 -0500

    added the stuff

---
+++ src/kernel/main.asm
@@ -0,0 +1,50 @@
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+    jmp .loop
+.done:
+    pop ax
+    pop si
+    ret
+
+
+
+main:
+    
+    ;setup data segments here and my rabbit thumped while typing this for no reason
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    ;hi
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov si, msg_hello
+    call puts
+    
+.halt:
+    jmp .halt
+
+
+msg_hello: db 'Booting up CrimsonOS...', ENDL, 0
+times 510-($-$$) db 0
+dw 0AA55h
